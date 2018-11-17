85a3898a88566f03c85275c9b4e1345e2f8b709c
Raghu <raghu@Raghus-Mac-Pro.local>
2018-11-17 10:52:45 +0530

    Added optional var

---
+++ 2/src/optional.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    // To define the variable as optional
+
+    var name1: String?
+    name1 = null
+    println(name1)
+
+    /*
+     * For non optional varaibles assigning the value to NULL is not possible
+     * var name2: String
+     * name2 = null
+     */
+
+
+
+
+
+
+}
