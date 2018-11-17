062d1cd7e6906b269bcad1c9e5d103b8e942719e
Raghu <raghu@Raghus-Mac-Pro.local>
2018-11-17 11:43:03 +0530

    conversion

---
+++ 3/src/conversion.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    var str1: String
+    var int1: Int
+
+    str1 = "1978"
+    int1 = str1.toInt()
+
+    println("Year " + int1)
+
+    var db1: Double = int1.toDouble()
+    println("Year " + db1)
+
+    db1 = 1978.1978
+    int1 = db1.toInt()
+
+    println("Year " + int1)
+
+
+
+}
