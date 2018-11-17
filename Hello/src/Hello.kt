9f538b1f92804553aac2392808c79e1d93eb0a30
Raghu <raghu@Raghus-Mac-Pro.local>
2018-11-17 10:39:01 +0530

    var def

--- Hello/src/Hello.kt
+++ Hello/src/Hello.kt
@@ -1,10 +1,20 @@
 fun main(args: Array<String>) {
 
-   var name = "Hello Raghu Ram"
+    // Your name
+
+    /*
+     * To define the type
+     * var name: String 
+     */
+
+    var name = "Hello Raghu Ram"
 
 
     println(name)
 
+    /*
+     * year you have born
+     */
     var year = 1978
 
     println(year)
